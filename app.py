8145d91471ea5c8cdeaf6a9645c6cdaea5cf2c5e
voycebutler18 <voycebutler1@gmail.com>
2025-07-11 18:05:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ API_KEY = os.getenv("GOOGLE_API_KEY")
 SEARCH_ENGINE_ID = os.getenv("GOOGLE_CSE_ID")
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
     
 @app.route('/about')
