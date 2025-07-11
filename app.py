21482385f3e6f517ff4ef06153d394cc4109b128
voycebutler18 <voycebutler1@gmail.com>
2025-07-10 22:54:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,7 @@ def generate_grant_response():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
