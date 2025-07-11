731d49bbb36389756be63d28cebca0a1035783da
voycebutler18 <voycebutler1@gmail.com>
2025-07-11 14:35:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -382,6 +382,16 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+@app.route('/')
+def home():
+    """Main page with form to submit grant requests"""
+    return render_template('index.html')  # NOT 'home.html'
+
+@app.route('/')
+def home():
+    """Main page with form to submit grant requests"""
+    return render_template('home.html')
+    
 
 @app.route('/generate', methods=['POST'])
 def generate_grant_response():
